34bd92f109741c986330762f082b139b079536f6
Sukhwinder Singh <44182331+c0736490@users.noreply.github.com>
2019-11-23 16:55:35 -0500

    Add files via upload

---
+++ WebForm7.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace projectapplied
+{
+    public partial class WebForm7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+          
+
+            }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+        
+    }
