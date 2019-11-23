bd3b2a1e0e01462c6955fcdfcfd49d4598d7f8fe
Sukhwinder Singh <44182331+c0736490@users.noreply.github.com>
2019-11-23 16:52:04 -0500

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace projectapplied
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+    }
+}
